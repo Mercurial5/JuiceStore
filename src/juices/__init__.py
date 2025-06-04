90f3009945cbcf476459061dd5b53b0b0e49c90f
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2025-06-04 16:44:30 +0500

    Add: initialization of db in juice feature

---
+++ src/juices/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+from dotenv import load_dotenv
+
+from .model import Juice
+from ..database.database import Database
+from ..database.settings import DatabaseSettings
+
+
+def get_database():
+    load_dotenv()
+
+    db_settings = DatabaseSettings(
+        host=os.getenv("JUICE_DB_HOST"),
+        port=int(os.getenv("JUICE_DB_PORT")),
+        username=os.getenv("JUICE_DB_USERNAME"),
+        password=os.getenv("JUICE_DB_PASSWORD"),
+        database=os.getenv("JUICE_DB_NAME"),
+    )
+    return Database(db_settings)
+
+
+db = get_database()
+
+
+def get_db_session():
+    return db.session
